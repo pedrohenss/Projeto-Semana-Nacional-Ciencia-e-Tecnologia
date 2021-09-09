aeb0adecde00dae4f7f4e668246ee40647b8f3ed
Pedro Henrique Santos <pxdr21@hotmail.com>
2021-09-09 00:51:49 -0300

    Initial commit

---
+++ quiz.py
@@ -0,0 +1,352 @@
+
+print ( '''
+           Projeto Semana Nacional da Ciência e Tecnologia 
+                        Passa ou Repassa Quiz                
+''')
+
+print ('''Questão n°1
+       ''')
+
+print ('Qual é o coletivo de cães?')
+print ('a) Matilha')
+print ('b) Rebanho')
+print ('c) Alcateia')
+print ('''d) Manada
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+pontos = 0
+
+if resposta == 'a':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°2
+       ''')
+
+print ('Quantos dias tem um ano bissexto?')
+print ('a) 350')
+print ('b) 364')
+print ('c) 365')
+print ('''d) 366
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'd':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°3
+       ''')
+
+print ('Qual foi o último Presidente do período da ditadura militar no Brasil?')
+print ('a) Costa e Silva')
+print ('b) João Figueiredo')
+print ('c) Ernestro Geisel')
+print ('''d) Emílio Médici
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'b':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°4
+       ''')
+
+print ('Qual o maior osso do corpo humano?')
+print ('a) Escápula')
+print ('b) Braço')
+print ('c) Tíbia')
+print ('''d) Fêmur
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'd':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°5
+       ''')
+
+print ('Quantos centímetros têm um metro?')
+print ('a) 10')
+print ('b) 1000')
+print ('c) 100')
+print ('''d) 10000
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'c':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°6
+       ''')
+
+print ('O que está escrito na bandeira do Brasil?')
+print ('a) Ordem em Progresso')
+print ('b) Ordem com Progresso')
+print ('c) Brasil acima de todos')
+print ('''d) Ordem e Progresso
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'd':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°7
+       ''')
+
+print ('Qual é o país conhecido por ter o formato de uma bota?')
+print ('a) França')
+print ('b) Alemanha')
+print ('c) Itália')
+print ('''d) Suíça
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'c':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°8
+       ''')
+
+print ('Quais são as cores primárias?')
+print ('a) Azul, verde e amarelo')
+print ('b) Verde, laranja e vermelho')
+print ('c) Azul, vermelho e amarelo')
+print ('''d) Vermelho, amarelo e verde
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'c':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°9
+       ''')
+
+print ('Em qual país foi construído o campo de concentração de Auschwitz, maior da Alemanha Nazista?')
+print ('a) Alemanha')
+print ('b) Áustria')
+print ('c) Polônia')
+print ('''d) França
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'c':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°10
+       ''')
+
+print ('Qual o idioma falado na Angola?')
+print ('a) Inglês')
+print ('b) Português')
+print ('c) Francês')
+print ('''d) Angolano
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'b':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°11
+       ''')
+
+print ('Quantas consoantes tem a palavra desenvolvimento?')
+print ('a) 15')
+print ('b) 9')
+print ('c) 6')
+print ('''d) 8
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'b':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print ('''
+Questão n°12
+       ''')
+
+print ('Qual inseto transmite a doença de Chagas?')
+print ('a) Aedes aegypti')
+print ('b) Barbeiro')
+print ('c) Barata')
+print ('''d) Percevejo
+       ''')
+
+resposta = input ('Digite uma das opções: ')
+
+if resposta == 'b':
+    print ('Certa resposta!')
+    pontos += 10             #pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print ('Você errou!')
+
+    print (f'Pontos acumulados até o momento: {pontos}')
+
+print('''
+Questão n°13
+         ''')
+
+print('Qual o feriado comemorado no dia 15 de novembro?')
+print('a) Independência do Brasil')
+print('b) Dia de Finados')
+print('c) Proclamação da República')
+print('''d) Dia da Consciência Negra
+      ''')
+
+resposta = input('Digite uma das opções: ')
+
+if resposta == 'c':
+    print('Certa resposta!')
+    pontos += 10  # pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print('Você errou!')
+
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+print('''
+Questão n°14
+         ''')
+
+print('Qual desses filmes nunca ganhou o Oscar de melhor filme?')
+print('a) 12 Anos de Escravidão')
+print('b) A Forma Da Água')
+print('c) Birdman')
+print('''d) Avatar
+      ''')
+
+resposta = input('Digite uma das opções: ')
+
+if resposta == 'd':
+    print('Certa resposta!')
+    pontos += 10  # pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print('Você errou!')
+
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+print('''
+Questão n°15
+         ''')
+
+print('Qual é a capital dos Estados Unidos?')
+print('a) Nova York')
+print('b) Washington')
+print('c) Los Angeles')
+print('''d) Chicago
+      ''')
+
+resposta = input('Digite uma das opções: ')
+
+if resposta == 'b':
+    print('Certa resposta!')
+    pontos += 10  # pontos = pontos + 10
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+else:
+    print('Você errou!')
+
+    print(f'Pontos acumulados até o momento: {pontos}')
+
+
